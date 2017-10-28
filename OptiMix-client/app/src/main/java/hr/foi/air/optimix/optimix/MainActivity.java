daf7394b18b28586a8663879d8307ab710cc6476
Filip Babić <filbabic@foi.hr>
2017-10-28 19:32:36 +0200

    Integracija butterknife-a

--- OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/MainActivity.java
+++ OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/MainActivity.java
@@ -1,7 +1,12 @@
 package hr.foi.air.optimix.optimix;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @OnClick(R.id.button)
+    public void mainButtonClick(View view){
+        Intent intent = new Intent(this, DetailsActivity.class);
+        startActivity(intent);
+    }
+
+    // event handler
+    View.OnClickListener imageButtonClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(), "Hello Android!", Toast.LENGTH_SHORT).show();
+        }
+    };
 }

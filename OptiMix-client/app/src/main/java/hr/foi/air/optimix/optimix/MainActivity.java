739c03d6e5b5352fe06331b9a90e074770f4cb3f
Filip Babić <filbabic@foi.hr>
2017-10-30 23:49:52 +0100

    Dodan je floating button. Pritiskom na floating button otvara se nova aktivnost koja je zasad prazna a u kojoj ce se dodavati korisnici.

--- OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/MainActivity.java
+++ OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/MainActivity.java
@@ -1,7 +1,11 @@
 package hr.foi.air.optimix.optimix;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @OnClick(R.id.floatingSettingsButton)
+    public void settingsButtonClicked(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
--- OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/SettingsActivity.java
+++ OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/SettingsActivity.java
@@ -3,10 +3,11 @@ package hr.foi.air.optimix.optimix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DetailsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
     }
 }

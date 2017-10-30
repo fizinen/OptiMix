ebaabea3d8bb937568a61b8f0baed30aa82ac81f
Filip Babić <filbabic@foi.hr>
2017-10-30 23:03:25 +0100

    Pritiskom na gumb prijava pokrece se glavna aktivnost.

--- OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/MainActivity.java
+++ OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/MainActivity.java
@@ -1,31 +1,13 @@
 package hr.foi.air.optimix.optimix;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-
-import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
-    @OnClick(R.id.button)
-    public void mainButtonClick(View view){
-        Intent intent = new Intent(this, DetailsActivity.class);
-        startActivity(intent);
-    }
-
-    // event handler
-    View.OnClickListener imageButtonClicked = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Toast.makeText(getApplicationContext(), "Hello Android!", Toast.LENGTH_SHORT).show();
-        }
-    };
 }

4380323be102af4c2c8b8d241febbf8aadd77831
Gloria Babić <globabic@foi.hr>
2017-10-31 17:58:36 +0100

    Dodan Person model

--- OptiMix-client/model/src/main/java/hr/foi/air/optimix/model/Person.java
+++ OptiMix-client/model/src/main/java/hr/foi/air/optimix/model/Person.java
@@ -7,4 +7,47 @@ import java.io.Serializable;
  */
 
 public class Person implements Serializable {
+
+    long idPerson;
+    String username;
+    String password;
+
+    public Person() {
+    }
+
+    public Person(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Person(long idPerson) {
+        this.idPerson = idPerson;
+    }
+
+    public long getIdPerson() {
+        return idPerson;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return this.username + " " + this.password;
+    }
+
+
 }

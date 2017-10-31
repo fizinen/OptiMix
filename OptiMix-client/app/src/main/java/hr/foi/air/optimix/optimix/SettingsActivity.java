50069fa41d3e3e6fd90b20086a6abae3928ed37b
Bodavk <bogerdelj@foi.hr>
2017-10-31 12:24:49 +0100

    Dodane su tab kontrole na activity koji se otvara pritskom na "settings" floating gumb. Osim toga, izmijenjena je ikona na floating buttonu (izgenerirane su u pluginu sve ikone pa su i one dodane u projekt)

--- OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/SettingsActivity.java
+++ OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/SettingsActivity.java
@@ -8,6 +8,6 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_user);
+        setContentView(R.layout.activity_settings);
     }
 }

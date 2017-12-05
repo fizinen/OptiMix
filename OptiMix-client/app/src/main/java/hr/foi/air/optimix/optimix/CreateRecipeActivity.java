4e6f2b39dc883f40796934858b39579207d44356
Bogdan Erdelji <bogerdelj@foi.hr>
2017-12-05 02:02:56 +0100

    https://trello.com/c/ACWxnaat Implementirano je generiranje elemenata za odabir materijalaod kojih se recept sastoji (na klik gumba). Još je potrebno povezati podatke u spinner elemente i isprogramirati pohranu unesenih podataka. Osim tog potrebno je srediti backstack

--- OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/CreateRecipeActivity.java
+++ OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/CreateRecipeActivity.java
@@ -1,13 +1,25 @@
 package hr.foi.air.optimix.optimix;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import hr.foi.air.optimix.core.Input;
 import hr.foi.air.optimix.model.Person;
 import hr.foi.air.optimix.optimix.handlers.CreateUserHandler;
@@ -21,8 +33,14 @@ import hr.foi.air.optimix.webservice.ServiceParams;
 
 public class CreateRecipeActivity extends AppCompatActivity {
 
-    @BindView(R.id.submitRecipeButton)
-    Button submitButton;
+    @BindView(R.id.submit_recipe_button) Button submitButton;
+    @BindView(R.id.add_new_material_button) Button newMaterialButton;
+    //Layout that we copy
+    @BindView(R.id.material_addition_layout) ViewGroup newMaterialAdditionLayout;
+    // Insert point
+    @BindView(R.id.recipe_details_layout) ViewGroup recipeDetailsLayout;
+
+    private int materialAddedCounter;
 
     public CreateRecipeActivity(){
     }
@@ -31,14 +49,16 @@ public class CreateRecipeActivity extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_recipe);
-       // submitButton.setOnClickListener(onSubmit);
+        ButterKnife.bind(this);
+        materialAddedCounter = 0;
+        newMaterialButton.setOnClickListener(onMaterialAdded);
+        submitButton.setOnClickListener(onSubmit);
     }
-
     View.OnClickListener onSubmit = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-
-           /*
+            Log.d("klik","kliknut gumb submit");
+/*
             //This part is the same as in CreateUserActivity - it has to be edited and done in such
             // way so that it works for recipes
             String nameValue = personName.getText().toString();
@@ -56,6 +76,18 @@ public class CreateRecipeActivity extends AppCompatActivity {
                         getString(hr.foi.air.optimix.webservice.R.string.person_createuser_path),
                         ServiceCaller.HTTP_POST, person));
             }*/
+        }
+    };
+    View.OnClickListener onMaterialAdded = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            // Here we have to add dynamical generation of Layouts such as is @id/material_addition_layout
+
+            LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            ViewGroup parent = (ViewGroup) findViewById(R.id.insertion_point);
+            View view = inflater.inflate(R.layout.layout_material_addition, null);
+            parent.addView(view, materialAddedCounter);
+            materialAddedCounter++;
         }
     };
 }

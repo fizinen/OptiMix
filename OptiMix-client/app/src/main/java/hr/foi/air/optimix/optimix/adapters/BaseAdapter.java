6ebbb41af04b292fd4a009196e9b8ab2c48e9989
Gloria Babić <globabic@foi.hr>
2017-12-05 01:25:15 +0100

    https://trello.com/c/s5AzbqL7 dodani adapteri za listu korisnika

---
+++ OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/adapters/BaseAdapter.java
@@ -0,0 +1,57 @@
+package hr.foi.air.optimix.optimix.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created by Gloria Babić on 4.12.2017..
+ */
+
+public abstract class BaseAdapter<T extends Serializable> extends ArrayAdapter<T> {
+
+    private LayoutInflater inflater;
+    private ArrayList<T> items;
+
+    public BaseAdapter(Context context, int resource, ArrayList<T> items) {
+        super(context, resource);
+        this.items = items;
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public T getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public int getPosition(T item) {
+        for(int i = 0; i < items.size(); i++) {
+            if(items.get(i) == item) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public LayoutInflater getInflater() {
+        return inflater;
+    }
+
+    public ArrayList<T> getItems() {
+        return items;
+    }
+}
---
+++ OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/adapters/UserAdapter.java
@@ -0,0 +1,61 @@
+package hr.foi.air.optimix.optimix.adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import hr.foi.air.optimix.model.Person;
+import hr.foi.air.optimix.optimix.R;
+
+/**
+ * Created by Gloria Babić on 2.12.2017..
+ */
+
+public class UserAdapter extends hr.foi.air.optimix.optimix.adapters.BaseAdapter<Person>{
+
+    public UserAdapter(Context context, int resource, ArrayList<Person> items) {
+        super(context, resource, items);
+    }
+
+
+    public static class ViewHolder {
+        public TextView personName;
+        public TextView personSurname;
+        public TextView personAuthority;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View vi = convertView;
+        final ViewHolder holder;
+        try {
+            if (convertView == null) {
+                vi = getInflater().inflate(R.layout.list_item, null);
+                holder = new ViewHolder();
+                holder.personName = (TextView) vi.findViewById(R.id.person_name);
+                holder.personSurname = (TextView) vi.findViewById(R.id.person_surname);
+                holder.personAuthority = (TextView) vi.findViewById(R.id.person_authority);
+
+                vi.setTag(holder);
+            } else {
+                holder = (ViewHolder) vi.getTag();
+            }
+
+            Person current = getItems().get(position);
+            holder.personName.setText(current.getName());
+            holder.personAuthority.setText(current.getAuthorityString());
+            holder.personSurname.setText(current.getSurname());
+
+
+        } catch (Exception e) {
+            Log.d("greškica", "ups");
+        }
+        return vi;
+    }
+}

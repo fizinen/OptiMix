eed3de0e1773a7ac958aadcfb479337957649e03
Filip Babić <filbabic@foi.hr>
2017-12-07 16:38:35 +0100

    U service dodan Analysis model, controller i repository za analize sirovina

---
+++ OptiMix-service/src/main/java/org/foi/air/optimix/repositories/AnalysisRepository.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.foi.air.optimix.repositories;
+
+import javax.persistence.Table;
+import org.foi.air.optimix.model.Analysis;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Lenovo
+ */
+@Repository
+@Table(name = "analysis")
+public interface AnalysisRepository extends JpaRepository<Analysis, String> {
+    
+    public Analysis findByIdAnalysis(long id);
+
+
+}

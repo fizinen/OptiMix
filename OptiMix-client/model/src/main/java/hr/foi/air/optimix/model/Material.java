a8b3782dba80fceb606d49e1d318d99f563a7b3e
Gloria Babić <globabic@foi.hr>
2017-12-10 02:08:59 +0100

    Dodan serializable kako bi se poklapao sa handlerom

--- OptiMix-client/model/src/main/java/hr/foi/air/optimix/model/Material.java
+++ OptiMix-client/model/src/main/java/hr/foi/air/optimix/model/Material.java
@@ -1,8 +1,10 @@
 package hr.foi.air.optimix.model;
 
+import java.io.Serializable;
+
 /**
  * Created by Gloria Babić on 10.12.2017..
  */
 
-public class Material {
+public class Material implements Serializable {
 }

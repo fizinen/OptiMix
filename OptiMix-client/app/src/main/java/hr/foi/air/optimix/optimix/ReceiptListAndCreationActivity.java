3d9d7cb2491ac7e5be847d1bea54927340f644ea
Gloria Babić <globabic@foi.hr>
2017-12-10 12:36:29 +0100

    Poravnanje koda

--- OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/ReceiptListAndCreationActivity.java
+++ OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/ReceiptListAndCreationActivity.java
@@ -58,7 +58,7 @@ public class ReceiptListAndCreationActivity extends Fragment implements View.OnC
         recipes = (ListView) view.findViewById(R.id.listViewRecipes);
 
         ServiceParams params = new ServiceParams(
-                getString( R.string.all_recipes_path) ,
+                getString(R.string.all_recipes_path),
                 ServiceCaller.HTTP_GET, null);
         new ServiceAsyncTask(recipeListHandler).execute(params);
 
@@ -69,11 +69,11 @@ public class ReceiptListAndCreationActivity extends Fragment implements View.OnC
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //We have to get the id of the chosen recipe so first we need an object
-                Recipe theChosenRecipe  = (Recipe) parent.getItemAtPosition(position);
+                Recipe theChosenRecipe = (Recipe) parent.getItemAtPosition(position);
                 long idOfRecipe = theChosenRecipe.getIdRecipe();
 
                 Intent intent = new Intent(getContext(), RecipeDetailActivity.class);
-                intent.putExtra("idRecipe",idOfRecipe);
+                intent.putExtra("idRecipe", idOfRecipe);
                 startActivity(intent);
             }
         });
@@ -84,13 +84,14 @@ public class ReceiptListAndCreationActivity extends Fragment implements View.OnC
     public void openNewRecipeCreationActivity() {
         Activity parentActivity = getActivity();
         Intent intent = new Intent(parentActivity, CreateRecipeActivity.class);
-        ((FragmentIntentInterface)parentActivity).startIntentFromFragment(intent);
+        ((FragmentIntentInterface) parentActivity).startIntentFromFragment(intent);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.floatingActionButtonAddNewRecipe: openNewRecipeCreationActivity();
+        switch (v.getId()) {
+            case R.id.floatingActionButtonAddNewRecipe:
+                openNewRecipeCreationActivity();
                 break;
         }
     }
@@ -98,8 +99,9 @@ public class ReceiptListAndCreationActivity extends Fragment implements View.OnC
     SimpleResponseHandler recipeListHandler = new SimpleResponseHandler() {
         @Override
         public boolean handleResponse(ServiceResponse response) {
-            if(response.getHttpCode() == 200) {
-                Type listType = new TypeToken<ArrayList<Recipe>>() { }.getType();
+            if (response.getHttpCode() == 200) {
+                Type listType = new TypeToken<ArrayList<Recipe>>() {
+                }.getType();
                 ArrayList<Recipe> t = new Gson().fromJson(response.getJsonResponse(), listType);
                 recipes.setAdapter(new RecipeAdapter(getActivity().getApplicationContext(),
                         R.layout.fragment_recipes_overview, t));

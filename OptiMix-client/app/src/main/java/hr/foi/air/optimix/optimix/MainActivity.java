4b8d7f3fc47c6bbb68b7285d1f056471d81a27b8
Gloria Babić <globabic@foi.hr>
2018-01-18 00:33:27 +0100

    https://trello.com/c/ycAYclZp Dodavanje logout opcije

--- OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/MainActivity.java
+++ OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/MainActivity.java
@@ -1,5 +1,7 @@
 package hr.foi.air.optimix.optimix;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -8,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import butterknife.OnClick;
+import hr.foi.air.optimix.core.SessionManager;
 
 public class MainActivity extends AppCompatActivity implements FragmentIntentInterface {
 
@@ -44,4 +47,46 @@ public class MainActivity extends AppCompatActivity implements FragmentIntentInt
         startActivity(intent);
 
     }
-}
+
+    @Override
+    public void onBackPressed() {
+        showPopUp();
+
+    }
+
+    private void showPopUp() {
+
+        AlertDialog.Builder helpBuilder = new AlertDialog.Builder(this);
+        helpBuilder.setTitle("Log out");
+        helpBuilder.setMessage("Da li se uistinu želite odjaviti?");
+        helpBuilder.setPositiveButton("Da",
+                new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        signOut();
+                    }
+                });
+
+        helpBuilder.setNegativeButton("Ne", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Zatvori dijalog
+            }
+        });
+
+        AlertDialog helpDialog = helpBuilder.create();
+        helpDialog.show();
+
+    }
+
+    private void signOut() {
+                SessionManager.getInstance(getApplicationContext()).destroyAll();
+                for (int i = 0; i < getFragmentManager().getBackStackEntryCount(); ++i) {
+                    getFragmentManager().popBackStack();
+                }
+                MainActivity.super.onBackPressed();
+            }
+
+
+    }

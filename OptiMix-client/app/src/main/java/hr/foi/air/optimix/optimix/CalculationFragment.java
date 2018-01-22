bb906497a18857976cb5b0dc04441a322b2990f0
Filip Babić <filbabic@foi.hr>
2018-01-22 05:13:34 +0100

    https://trello.com/c/TcSQ73qx Napravljen xml za fragment za izračune i sama funckionalnost fragmenta

--- OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/CalculationFragment.java
+++ OptiMix-client/app/src/main/java/hr/foi/air/optimix/optimix/CalculationFragment.java
@@ -1,14 +1,18 @@
 package hr.foi.air.optimix.optimix;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,6 +32,8 @@ import hr.foi.air.optimix.model.Calculation;
 import hr.foi.air.optimix.model.CalculationAnalysis;
 import hr.foi.air.optimix.model.Recipe;
 import hr.foi.air.optimix.model.RecipeRaws;
+import hr.foi.air.optimix.optimix.adapters.CalculationAdapter;
+import hr.foi.air.optimix.optimix.adapters.CalculationAnalysisAdapter;
 import hr.foi.air.optimix.optimix.adapters.RecipeRawsAdapter;
 import hr.foi.air.optimix.optimix.adapters.SpinnerRecipeAdapter;
 import hr.foi.air.optimix.optimix.handlers.CreateCalculationAnalysisHandler;
@@ -47,18 +53,27 @@ public class CalculationFragment extends android.support.v4.app.Fragment {
 
     @BindView(R.id.recipe_spinner)
     Spinner chooseRecipe;
-    @BindView(R.id.make_calculation_button)
-    Button calculateRecipe;
+    @BindView(R.id.preview_of_calculations)
+    Button calculationPreview;
     @BindView(R.id.recipe_amount)
     EditText calculationAmount;
-    
+    @BindView(R.id.calculation_made_name)
+    TextView calculationRecipeMadeName;
+    @BindView(R.id.calculation_full_amount)
+    TextView calculationFullAmount;
+    @BindView(R.id.layout_recipe_calculation_view)
+    LinearLayout layoutRecipeCalculationView;
+
     boolean error = false;
     ArrayList<RecipeRaws> listOfRecipeRaws;
     ArrayList<Analysis> listOfAllAnalysis;
+    ArrayList<CalculationAnalysis> listOfCalculationAnalysis;
     Double calcAmount;
     Calculation calculation;
     Recipe recipe;
     CalculationAnalysis calculationAnalysis;
+    View view;
+    ListView listViewForPreview;
 
     public CalculationFragment(){
 
@@ -72,16 +87,18 @@ public class CalculationFragment extends android.support.v4.app.Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_calculation, container, false);
+
+        view = inflater.inflate(R.layout.fragment_calculation, container, false);
 
         ButterKnife.bind(this, view);
+
+        listViewForPreview = (ListView) view.findViewById(R.id.listViewRecipes);
 
         ServiceParams params = new ServiceParams(
                 getString(R.string.all_recipes_path),
                 ServiceCaller.HTTP_GET, null);
         new ServiceAsyncTask(recipeListHandler).execute(params);
-
-        calculateRecipe.setOnClickListener(onCalculate);
+        calculationPreview.setOnClickListener(onCalculationPreview);
 
         return view;
     }
@@ -108,37 +125,6 @@ public class CalculationFragment extends android.support.v4.app.Fragment {
         }
     };
 
-    View.OnClickListener onCalculate  = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-            recipe = (Recipe) chooseRecipe.getSelectedItem();
-            calcAmount = Double.parseDouble(calculationAmount.getText().toString());
-
-            calculation = new Calculation(calcAmount, recipe);
-
-            CreateCalculationHandler createCalculationHandler = new CreateCalculationHandler(getActivity(), calculation);
-
-            new ServiceAsyncTask(createCalculationHandler).execute(new ServiceParams(
-                    getString(hr.foi.air.optimix.webservice.R.string.calculation_create_path),
-                    ServiceCaller.HTTP_POST, calculation));
-
-            if (recipe.getIdRecipe() != -1) {
-                ServiceParams params = new ServiceParams(getString(hr.foi.air.optimix.webservice.R.string.raws_for_recipe_path) + recipe.getIdRecipe(),
-                        ServiceCaller.HTTP_POST, null);
-                new ServiceAsyncTask(recipeRawsListHandler).execute(params);
-            }
-
-            if (recipe.getIdRecipe() != -1) {
-                ServiceParams params = new ServiceParams(
-                        getString(R.string.all_analysis_path),
-                        ServiceCaller.HTTP_GET, null);
-                new ServiceAsyncTask(analysisListHandler).execute(params);
-            }
-
-        }
-    };
-
 
 
     SimpleResponseHandler recipeRawsListHandler = new SimpleResponseHandler() {
@@ -149,7 +135,6 @@ public class CalculationFragment extends android.support.v4.app.Fragment {
 
                 listOfRecipeRaws = new ArrayList<>(listar);
 
-                //Toast.makeText(getActivity(), "Dohvaćen recept", Toast.LENGTH_LONG).show();
                 return true;
 
             } else {
@@ -177,8 +162,6 @@ public class CalculationFragment extends android.support.v4.app.Fragment {
                         ServiceCaller.HTTP_GET, null);
                 new ServiceAsyncTask(calculationListHandler).execute(params);
 
-                //Toast.makeText(getActivity().getApplicationContext(), "Dohvaćene analize", Toast.LENGTH_LONG).show();
-
                 return true;
             } else {
                 Toast.makeText(getActivity().getApplicationContext(), "Failed to fetch analysis", Toast.LENGTH_LONG).show();
@@ -202,10 +185,10 @@ public class CalculationFragment extends android.support.v4.app.Fragment {
                         for (RecipeRaws rr : listOfRecipeRaws){
                             for (Analysis a : listOfAllAnalysis){
                                 if (rr.getRecipeRawId().getIdRaw() == a.getRawId().getIdRaw()){
-                                    double amountForCalculation = (rr.getRawAmount()/1000)*calcAmount;
+                                    double amountForCalculation = (rr.getRawAmount()/100)*calcAmount;
                                     if(amountForCalculation < a.getAnalysisRawMass()){
 
-                                        calculationAnalysis = new CalculationAnalysis(c.getIdCalculation(), c, a, amountForCalculation);
+                                        calculationAnalysis = new CalculationAnalysis(c.getRecipeId().getIdRecipe(), c, a, amountForCalculation);
 
                                         CreateCalculationAnalysisHandler createCalculationAnalysisHandler = new CreateCalculationAnalysisHandler(getActivity(), calculationAnalysis);
 
@@ -218,16 +201,100 @@ public class CalculationFragment extends android.support.v4.app.Fragment {
                                     }
                                     else{
                                         Toast.makeText(getActivity().getApplicationContext(), "Premalo analizirane sirovine za recept", Toast.LENGTH_LONG).show();
+                                        return true;
                                     }
                                 }
                             }
                         }
                     }
                 }
-
+                calculationPreview.performClick();
                 return true;
             } else {
                 Toast.makeText(getActivity().getApplicationContext(), "Failed to fetch calculation", Toast.LENGTH_LONG).show();
+                return false;
+            }
+
+        }
+    };
+
+    View.OnClickListener onCalculationPreview  = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            recipe = (Recipe) chooseRecipe.getSelectedItem();
+
+            layoutRecipeCalculationView.setVisibility(View.VISIBLE);
+
+            if (recipe.getIdRecipe() != -1) {
+                ServiceParams params = new ServiceParams(
+                        getString(R.string.all_calculationanalysis_path),
+                        ServiceCaller.HTTP_GET, null);
+                new ServiceAsyncTask(calculationAnalysisHandler).execute(params);
+            }
+
+        }
+    };
+
+    SimpleResponseHandler calculationAnalysisHandler = new SimpleResponseHandler() {
+        @Override
+        public boolean handleResponse(ServiceResponse response) {
+            if (response.getHttpCode() == 200) {
+
+                recipe = (Recipe) chooseRecipe.getSelectedItem();
+                calcAmount = Double.parseDouble(calculationAmount.getText().toString());
+
+                List<CalculationAnalysis> calculationAnalysises = new Gson().fromJson(response.getJsonResponse(), new TypeToken<List<CalculationAnalysis>>(){}.getType());
+
+                listOfCalculationAnalysis = new ArrayList<>(calculationAnalysises);
+
+                ArrayList<CalculationAnalysis> t = new ArrayList<>();
+
+                for(CalculationAnalysis c :listOfCalculationAnalysis){
+                    if(c.getCalculationId() == recipe.getIdRecipe() && c.getCalculationAnalysisId().getCalculationFullAmount() == calcAmount){
+                        t.add(c);
+                    }
+                }
+
+                if(t.size() > 0){
+                    calculationRecipeMadeName.setText(recipe.getRecipeName());
+                    calculationFullAmount.setText(String.valueOf(calcAmount));
+
+                    listViewForPreview.setAdapter(new CalculationAnalysisAdapter(getActivity().getApplicationContext(),
+                            R.layout.fragment_calculation, t));
+
+                    return true;
+                }
+                else{
+                    recipe = (Recipe) chooseRecipe.getSelectedItem();
+                    calcAmount = Double.parseDouble(calculationAmount.getText().toString());
+
+                    calculation = new Calculation(calcAmount, recipe);
+
+                    CreateCalculationHandler createCalculationHandler = new CreateCalculationHandler(getActivity(), calculation);
+
+                    new ServiceAsyncTask(createCalculationHandler).execute(new ServiceParams(
+                            getString(hr.foi.air.optimix.webservice.R.string.calculation_create_path),
+                            ServiceCaller.HTTP_POST, calculation));
+
+                    if (recipe.getIdRecipe() != -1) {
+                        ServiceParams params = new ServiceParams(getString(hr.foi.air.optimix.webservice.R.string.raws_for_recipe_path) + recipe.getIdRecipe(),
+                                ServiceCaller.HTTP_POST, null);
+                        new ServiceAsyncTask(recipeRawsListHandler).execute(params);
+                    }
+
+                    if (recipe.getIdRecipe() != -1) {
+                        ServiceParams params = new ServiceParams(
+                                getString(R.string.all_analysis_path),
+                                ServiceCaller.HTTP_GET, null);
+                        new ServiceAsyncTask(analysisListHandler).execute(params);
+                    }
+
+                    return true;
+                }
+
+            } else {
+                Toast.makeText(getActivity(), "Failed to fetch reciperaws", Toast.LENGTH_LONG).show();
                 return false;
             }
 
